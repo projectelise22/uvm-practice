d817e8d97abd0c2879e789d30451f9016cb2d373
Dolly Palencia <dollypalencia.work@gmail.com>
2026-01-28 18:09:18 +0200

    Added tb files

---
+++ uvm-reporting/testbench_uvm_action.sv
@@ -0,0 +1,38 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class driver extends uvm_driver;
+    `uvm_component_utils(driver);
+
+    function new(string path, uvm_component parent);
+        super.new(path, parent);
+    endfunction
+
+    task run();
+        `uvm_info("DRV", "This is an information message from driver", UVM_NONE);
+        `uvm_warning("DRV", "This is a warning message from driver");
+        `uvm_error("DRV", "This is an error message from driver");
+        #10;
+        `uvm_fatal("DRV", "This is a fatal message from driver");
+    endtask
+endclass
+
+module tb;
+    driver drv;
+
+    initial begin
+        drv = new("DRV", null);
+
+        // UVM_INFO should not display
+        drv.set_report_severity_action(UVM_INFO, UVM_NO_ACTION);
+      
+        // Change UVM_FATAL to not terminate immediately
+        drv.set_report_severity_action(UVM_FATAL, UVM_DISPLAY);
+        drv.run();
+      
+        // UVM_INFO will display and exit after running task
+        #10;
+        drv.set_report_severity_action(UVM_INFO, UVM_DISPLAY | UVM_EXIT);
+        drv.run();
+    end
+endmodule
--- uvm-reporting/testbench_uvm_err_warn_fatal.sv
+++ uvm-reporting/testbench_uvm_err_warn_fatal.sv
@@ -16,6 +16,8 @@ class drv extends uvm_driver;
     #10;
     // simulation stops, $finish will be called
     `uvm_fatal("DRV", "This is a fatal message from driver");
+    #10;
+    `uvm_fatal("DRV1", "This is a fatal message from driver");
   endtask
 endclass
 
@@ -24,7 +26,26 @@ module tb;
   
   initial begin
     tb_drv = new("DRV", null);
+    // shows other reporting mechanism: warning, error, fatal
     tb_drv.set_report_verbosity_level(UVM_HIGH);
-    tb_drv.run();
+    // tb_drv.run();
+    
+    // override by severity
+    // changing from fatal to error
+    // should report as ERROR and not call $finish
+    // tb_drv.set_report_severity_override(UVM_FATAL, UVM_ERROR);
+    // tb_drv.run();
+    
+    // override by severity id
+    // changing from fatal drv to error drv
+    // should report as error while drv1 remains as fatal
+    tb_drv.set_report_severity_id_override(UVM_FATAL, "DRV", UVM_ERROR);
+    tb_drv.run();    
+    // Log result:
+    // # KERNEL: UVM_INFO /home/runner/testbench.sv(13) @ 0: DRV [DRV] This message reports from driver
+    // # KERNEL: UVM_WARNING /home/runner/testbench.sv(14) @ 0: DRV [DRV] This is a warning message from driver
+    // # KERNEL: UVM_ERROR /home/runner/testbench.sv(15) @ 0: DRV [DRV] This is an error message from driver
+    // # KERNEL: UVM_ERROR /home/runner/testbench.sv(18) @ 10: DRV [DRV] This is a fatal message from driver
+    // # KERNEL: UVM_FATAL /home/runner/testbench.sv(20) @ 20: DRV [DRV1] This is a fatal message from driver
   end
 endmodule
